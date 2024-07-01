63afd2e828af9dc56243ac60e59ef356f4623025
Kazybaeva Altyn-Ai <golden.job010189@gmail.com>
2024-07-01 17:09:46 +0600

    altyn-ai project

--- secondPage/js.js
+++ secondPage/js.js
@@ -1,5 +1,14 @@
 let popup = document.querySelector('.popup')
 
-  let popupToggle = () => {
+  let popupTogle= () => {
     popup.classList.toggle('active')
   }
+
+  let ul = document.querySelector('header ul' )
+  let burger = document.querySelector('.burger')
+
+  let menuBurger = () =>{
+    ul.classList.toggle('active')
+    burger.classList.toggle('active')
+  }
+ 
